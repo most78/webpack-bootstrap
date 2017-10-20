e44bd6a88ee9d69ef9fda075ace1d93ca8034bba
Marcos Ostos <msanchez@qdqmedia.com>
2017-10-20 10:14:18 +0200

    cleaning project for next chapter

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import logo from './webpack-logo.png';
-import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  var myLogo = new Image();
-  myLogo.src = logo;
-
-  element.appendChild(myLogo);
-
-  console.log(Data);
 
   return element;
 }
